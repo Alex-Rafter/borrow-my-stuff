484704185bc820aace58d0bcf04e28428c39b14b
Alex Rafter <alexrafteremail@gmail.com>
2022-01-22 21:18:03 +0000

    experiments with routes and middleware

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,55 @@
 const express = require('express')
 const router = express.Router()
 
-// router.route('*/:id').get((req, res) => {
+router.use(logger)
+
+// router
+//   .route('*/:id')
+//   .get((req, res) => {
 //     res.send(`user detail: ${req.params.id}`)
+
+//   })
+//   .put((req, res) => {
+//     res.send(`user detail: ${req.params.id}`)
+//   })
+//   .delete((req, res) => {
+//     res.send(`user detail: ${req.params.id}`)
+//   })
+  
+const users = [{ name: 'sally' }, { name: 'john' }]
+
+
+
+// router.param('id', (req, res, next, id) => {
+//   req.user = users[id]
+//   next()
 // })
 
-router.get('/', (req, res) => {
-  res.send('user list')
-})
+function logger(req, res, next) {
+  console.log(req.originalUrl)
+  next()
+}
+
+
+// router.get('/', (req, res) => {
+//   res.send('user list')
+// })
 
 router.get('/new', (req, res) => {
-    res.send('new user form')
-  })
+  console.log(req.query.name)
+  res.render('users/new', {firstName: 'John', lastName: 'Doe'})
+})
 
-// router.post('/', (req, res) => {
-//    res.send('create user')     
-// })
+router.post('/', (req, res) => {
+  console.log(req.body.firstName)
+   res.send('create user')
+})
 
 // dyanmic routes
 
-router.get('/:id', (req, res) => {
-    req.params.id
-    res.send(`user detail: ${req.params.id}`)
-})
-
+// router.get('/:id', (req, res) => {
+//   req.params.id
+//   res.send(`user detail: ${req.params.id}`)
+// })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    console.log('Hello World')
-    res.render('index', {text : 'World'})
+  console.log('Hello World')
+  res.render('index', { text: 'World' })
 })
 
 const userRoute = require('./routes/users')
